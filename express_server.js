95bc83f111b409d5e57d7742194c33eb8dafd151
vagrant <kennethracadio@hotmail.com>
2018-01-23 21:05:43 +0000

    Installed body-pasrser and updated express server func

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var urlDatabase = {
