6a6c13649a85bb41f661a2165bb15278ccd961fc
vagrant <kennethracadio@hotmail.com>
2018-01-23 23:46:14 +0000

    Redirect short uls using res.redirect

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -45,9 +45,18 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// moved above "/urls/:id" because :id is a string and it'll redirect to "/urls/:id" first
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
+app.get("/u/:shortURL", (req, res) => {
+ // let longURL = ...
+ let shortURL = req.params.shortURL
+ let longURL = urlDatabase[shortURL]
+ res.redirect(longURL);
+});
+
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
@@ -58,8 +67,8 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // debug statement to see POST parameters
-  var shortURL = generateRandomString();
-  var longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
   // console.log(longURL);
   // console.log(shortURL);
   urlDatabase[shortURL] = longURL;
