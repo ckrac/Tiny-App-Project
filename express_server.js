a648404d8f23ec701150dc85cb2c8f87cb3f76f3
vagrant <kennethracadio@hotmail.com>
2018-01-26 21:51:46 +0000

    Cleaned up code

--- express_server.js
+++ express_server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session')
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
-const bodyParser = require("body-parser");
-// const cookieParser = require('cookie-parser')
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
-// app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   secret: 'key1'
+}));
 
-}))
 app.set("view engine", "ejs");
 
 let urlDatabase = {
@@ -54,23 +55,17 @@ const users = {
 }
 
 
-// function to add urls into userURLS ------------//
+//------------//
 function updateUserURLS (id) {
   const userURLS = {};
   for (let key in urlDatabase)
     if (urlDatabase[key].user_ID == id ) {
-      // console.log(urlDatabase[key])
       userURLS[key] = urlDatabase[key];
     }
-  // console.log(userURLS);
   return userURLS;
 }
 
-// console.log(updateUserURLS("ex"));
-
-
-
-// function to generate 6 random random alphanumeric ------------//
+//------------ used to generate userID and shortURLs//
 function generateRandomString() {
    const vocabulary = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'];
    let output = "";
@@ -86,8 +81,8 @@ function getRandomInt() {
    max = Math.floor(25);
    return Math.floor(Math.random() * (25 - 0)) + 0;
 }
-// function to check for duplicates in an object
-/////there is a find in method to check. change function later
+
+//------------ checks for a value duplicate//
 function checkDuplicate (obj, checkKey, checkValue) {
   for (let element in obj) {
     console.log(element);
@@ -98,32 +93,16 @@ function checkDuplicate (obj, checkKey, checkValue) {
   }
 
 }
-// console.log(checkDuplicate(users, "email", "user2@example.com" ));
-
-// if ((checkDuplicate(users, "email", "user2@example.com" ))) {
-//   console.log("x");
-// } else {
-//   console.log("hhhh")
-// }
-
-// function to find a user in an object by checking given email and password
+//------------//
 function findUser (obj, email) {
   let user_id;
-  // console.log(email);
-  // console.log(password);
   for (let element in obj) {
-  // console.log(obj[element].email);
-  // console.log(obj[element].password);
     if (obj[element].email === email) {
       user_id = obj[element].id;
       return user_id;
     }
   }
 }
-
-
-
-// console.log(findUser(users, "hello@example.com", "password"));
 
 //-------------------------------------------//
 
@@ -140,7 +119,7 @@ app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-// home page of index list
+//------------ home page of index list//
 app.get("/urls", (req, res) => {
   const user_id = req.session.user_id;
   const userURL = updateUserURLS(user_id);
@@ -154,7 +133,7 @@ app.get("/urls", (req, res) => {
   }
 });
 
-// ------- moved above "/urls/:id" because :id is a string and it'll redirect to "/urls/:id" first
+//------------ adds a new url to index page//
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase,
     user_id: req.session.user_id
@@ -167,7 +146,7 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-// ------- registration page
+//------------ registration page//
 app.get("/register", (req, res) => {
   let templateVars = { urls: urlDatabase,
     user_id: req.session.user_id
@@ -177,18 +156,13 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
   let userID = generateRandomString();
-  // console.log(userID);
-  // console.log(req.body);
   let user_email = req.body.email;
-  // console.log(email);
   let user_password = req.body.password;
   const hashedPassword = bcrypt.hashSync(user_password, 10);
   console.log(hashedPassword);
   if (!user_email || !hashedPassword)  {
-    // console.log("error");
     res.status(400).send("Error: 400 - Please submit both an email and password.");
   } else if (checkDuplicate(users, "email", user_email)) {
-    // console.log("x");
     res.status(400).send("Error: 400 - Email taken. Please use a new email.");
   } else {
     users[userID] = {
@@ -199,57 +173,40 @@ app.post("/register", (req, res) => {
     console.log(users);
     res.redirect("/urls");
   }
-    // console.log(users);
 });
 
-// ------- :redirects shortURL. it is equal to the key values (6 alpha letters)
+//------------ redirects shortURL. it is equal to the key values (6 alpha letters)//
 app.get("/u/:shortURL", (req, res) => {
- // shortURL is the key value of the object
-  // check what's req to access correct key
-  // console.log(req);
-  // req.params stores a list of values from ejs page
   let shortURL = req.params.shortURL
-  // console.log(req.params);
   let longURL = urlDatabase[shortURL].longURL
-  // console.log(longURL);
   // longURL is now set to the value(thelongurl) of the key(shortURL)
   // redirects to the page eg /u/9sm5xK for google
   // if i where to input the key value after /u/
   res.redirect(longURL);
 });
 
-
+//------------ assign a new longURL to shortURL//
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
     user_id: req.session.user_id,
     urls: urlDatabase
   };
-  // console.log(req.params);
-  // console.log(req.body);
   res.render("urls_show", templateVars);
 });
 
-// ------- post from form page to update urlDatabase
+//------------ post from form page to update urlDatabase//
 app.post("/urls", (req, res) => {
   // console.log(req.body);  // debug statement to see POST parameters
-  // object shows the key "longURL" because it the the name used in urls_new.ejs form //
   let shortURL = generateRandomString();
-  // to access value pair to the key longURL
   let longURL = req.body.longURL;
-  // console.log(longURL);
-  // console.log(shortURL);
   let user_id = req.session.user_id;
-  //// add user_id to string so url shows who submitted the url
+  //// updates urlDatabase
   urlDatabase[shortURL] = { "user_ID": user_id, "longURL": longURL }
-  console.log(urlDatabase);
-  // can redirect by using res.redirect(" -where to redirect after submit")
-  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
   res.redirect("/urls");
 });
 
-// ------- deletes a list in /urls
+//------------ deletes a list in /urls//
 app.post("/urls/:id/delete", (req, res) => {
-  // console.log(req.params);
   let remove = req.params.id;
   console.log(remove);
   const user_id = req.session.user_id;
@@ -262,7 +219,7 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-// ------- edits /urls with a new urls
+//------------ edits /urls with a new urls//
 app.post("/urls/:id", (req, res) => {
   let shortURL = req.params.id;
   let longURL = req.body.longURL;
@@ -276,7 +233,7 @@ app.post("/urls/:id", (req, res) => {
 });
 
 
-// ------- login page
+//------------ login page//
 app.get("/login", (req, res) => {
   let templateVars = { urls: urlDatabase,
     user_id: req.session.user_id
@@ -284,18 +241,14 @@ app.get("/login", (req, res) => {
   res.render("urls_login", templateVars);
 });
 
-// ------- generate cookie when user logs in
+// ------- generate cookie when user logs in//
 app.post("/login", (req, res) => {
   let user_email = req.body.email
-  // console.log(user_email);
   let user_password = req.body.password;
-  // console.log(user_password);
-  // console.log(checkDuplicate(users, "email", user_email));
-  // console.log(checkDuplicate(users, "password", user_password));
   // ----------- check if pass and email exist. if it does, search users and return userid as cookie ------
   if (checkDuplicate(users, "email", user_email)) {
-    // console.log("These exist");
     let user_id = findUser(users, user_email,);
+    // check if password matches id
     if (bcrypt.compareSync(user_password, users[user_id].password)) {
       req.session.user_id = users[user_id].id;
       res.redirect("/urls");
@@ -307,12 +260,9 @@ app.post("/login", (req, res) => {
   }
 });
 
-// ------- respons to a logout button that clears cookies and redirects back to /urls
+//------------ response to a logout button that clears cookies and redirects back to /urls
 app.post("/login/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
